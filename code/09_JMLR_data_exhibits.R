835aaf55151bd90db19045d67c876edfe70945ce
Dan <danieladamspencer@users.noreply.github.com>
2024-07-13 22:16:51 -0400

    Streamlined comparing the model performance on the JMLR data

--- code/09_JMLR_data_exhibits.R
+++ code/09_JMLR_data_exhibits.R
@@ -8,21 +8,22 @@ sim_data <- readRDS("data/1_BTRCP_JMLR_image_sim_data.rds")
 result_dir <- "~/github/BTRTucker/results/BTRCP_JMLR_simulations/"
 # Determine best result ----
 # Use DIC for the BTR CP and BTR Tucker models
-for(img_name in c("R3", "R5", "shapes", "hawk", "horse", "palm")) {
-  img_name <- "hawk"
+# for(img_name in c("R3", "R5", "shapes", "hawk", "horse", "palm")) {
+best_model_info <- lapply(c("R3", "R5", "shapes", "hawk", "horse", "palm"), function(img_name) {
+  # img_name <- "hawk"
   img <- as.matrix(sim_data[[img_name]])
-  # BTR CP
-  res_files <- list.files(result_dir, pattern = paste0("btrcp_",img_name), full.names = TRUE)
-  btrcp_dic <- sapply(res_files, function(x) {
-    btrcp_res <- readRDS(x)
-    DIC(btrcp_res$llik, burn_in = 300)})
-  btrcp_res <- readRDS(res_files[which.min(btrcp_dic)])
-  btrcp_B <- BTRT_final_B(btrcp_res)
-  # BTR Tucker
-  res_files <- list.files(result_dir, pattern = paste0("btrt_",img_name), full.names = TRUE)
-  btrt_dic <- sapply(res_files, function(x) {
-    btrt_res <- readRDS(x)
-    DIC(btrt_res$llik, burn_in = 300)})
-  btrt_res <- readRDS(res_files[which.min(btrt_dic)])
-  btrt_B <- BTRT_final_B(btrt_res)
-}
+  lapply(c("btrcp", "btrt"), function(model_name) {
+    res_files <- list.files(result_dir, pattern = paste0(model_name, "_", img_name, full.names = TRUE))
+    mdl_DIC <- sapply(res_files, function(x) {
+      bt_res <- readRDS(x)
+      DIC(bt_res$llik, burn_in = 300)
+    })
+    best_file <- res_files[which.min(mdl_DIC)]
+    best_res <- readRDS(best_file)
+    best_rank <- stringr::str_extract(best_files, pattern = "rank[0-9]+")
+    best_rank <- sub("rank", "", best_rank)
+    best_B <- BTRT_final_B(best_res)
+    best_RMSE <- sqrt(mean((best_B - img)^2))
+    list(B = best_B, Rank = best_rank, RMSE = best_RMSE)
+  })
+})
